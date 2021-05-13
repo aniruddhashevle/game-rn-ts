24f4ab300c7c66849d96cf486017fc800600b77c
Aniruddha Shevle <aniruddhashevle@gmail.com>
2021-05-13 19:52:41 +0800

    build(dev-infra): app rename

--- android/app/src/main/java/com/cardgame/MainActivity.java
+++ android/app/src/main/java/com/cardgame/MainActivity.java
@@ -1,4 +1,4 @@
-package com.t;
+package com.cardgame;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "t";
+    return "CardGame";
   }
 }
--- ios/CardGameTests/CardGameTests.m
+++ ios/CardGameTests/CardGameTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface tTests : XCTestCase
+@interface CardGameTests : XCTestCase
 
 @end
 
-@implementation tTests
+@implementation CardGameTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
