d4fd91d0bd276c85b9f84f1430a73e7f475cd050
Aniruddha Shevle <aniruddhashevle@gmail.com>
2021-05-17 02:17:21 +0800

    feat(core): flip flop anime

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import {
   Text,
   TouchableHighlight,
+  Animated,
+  View,
 } from 'react-native';
 import appColors from '../../config/appColors';
 import styles from './CardsStyle';
@@ -13,7 +15,7 @@ interface CardProps {
   onCardPress: null | ((cardId: number, cardNumber: number) => void);
 }
 
-const Card = (props: CardProps) => {
+const Card: React.FC<CardProps> = (props: CardProps) => {
   const {
     cardId,
     cardNumber,
@@ -21,15 +23,75 @@ const Card = (props: CardProps) => {
     onCardPress,
   } = props;
 
+  const animate = useRef(new Animated.Value(0));
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const doAFlip = () => {
+    if (!isRightCard) {
+      Animated.timing(animate.current, {
+        duration: 300,
+        toValue: isFlipped ? 0 : 180,
+        useNativeDriver: true,
+      }).start(() => {
+        setIsFlipped(!isFlipped);
+      });
+      onCardPress && onCardPress(cardId, cardNumber);
+    }
+  };
+
+  const interpolatedValueFront = animate.current.interpolate({
+    inputRange: [0, 180],
+    outputRange: ["0deg", "180deg"],
+  });
+
+  const interpolatedValueBack = animate.current.interpolate({
+    inputRange: [0, 180],
+    outputRange: ["180deg", "360deg"],
+  });
+
+  const rotateFront = {
+    transform: [
+      {
+        rotateY: interpolatedValueFront,
+      },
+    ],
+    ...styles.cardFace,
+    ...styles.frontCard,
+  };
+
+  const rotateBack = {
+    transform: [
+      {
+        rotateY: interpolatedValueBack,
+      },
+    ],
+    ...styles.cardFace,
+    ...styles.backCard,
+  };
+
   return (
-    <TouchableHighlight
-      activeOpacity={0.6}
-      underlayColor={appColors.cardSelect}
-      onPress={() => onCardPress && onCardPress(cardId, cardNumber)}
-      style={styles.card}
-    >
-      <Text style={styles.cardNumber}>{isRightCard ? `${cardNumber} RIGHT` : cardNumber}</Text>
-    </TouchableHighlight>
+    <View style={styles.cardContainer}>
+      <Animated.View style={[rotateFront, styles.fullCover, styles.card]}>
+        <TouchableHighlight
+          activeOpacity={0.6}
+          underlayColor={appColors.white}
+          onPress={doAFlip}
+          style={[styles.fullCover, styles.cardTouch]}
+        >
+          <Text style={styles.cardMark}>?</Text>
+        </TouchableHighlight>
+      </Animated.View>
+      <Animated.View style={[rotateBack, styles.fullCover, styles.card, styles.back]}>
+        <TouchableHighlight
+          activeOpacity={0.6}
+          underlayColor={appColors.white}
+          onPress={doAFlip}
+          style={[styles.fullCover, styles.cardTouch]}
+        >
+          <Text style={styles.cardNumber}>{cardNumber}</Text>
+        </TouchableHighlight>
+      </Animated.View>
+    </View>
   );
 };
 
--- src/components/Cards/CardsStyle.ts
+++ src/components/Cards/CardsStyle.ts
@@ -4,30 +4,56 @@ import {
 import appColors from '../../config/appColors';
 
 const styles = StyleSheet.create({
+  fullCover: {
+    width: '100%',
+    height: '100%',
+  },
   container: {
+    padding: 5,
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     flexWrap: 'wrap',
     alignContent: 'center',
-    padding: 5,
   },
-  card: {
+  cardContainer: {
     width: '30%',
     height: '22%',
     margin: 5,
-    borderRadius: 10,
-    borderColor: appColors.white,
-    borderWidth: 5,
-    flexGrow: 1,
-    flexShrink: 0,
-    backgroundColor: appColors.cardsBlue,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    backfaceVisibility: 'hidden',
   },
   cardNumber: {
-    fontSize: 20,
+    fontSize: 24,
+  },
+  cardMark: {
+    fontSize: 28,
+    color: appColors.white,
+  },
+  cardFace: {
+    borderWidth: 5,
+    borderRadius: 10,
+    borderColor: appColors.white,
+    flexGrow: 1,
+    flexShrink: 0,
+  },
+  frontCard: {
+    backgroundColor: appColors.cardsBlue,
+  },
+  backCard: {
+    backgroundColor: appColors.white,
+  },
+  back: {
+    position: 'absolute',
+    top: 0,
+  },
+  cardTouch: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
